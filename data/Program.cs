4f3d717e4e51ebaa286d7349b6f83a6e8a3cede3
Leandro <leandrorochaadm@gmail.com>
2020-03-18 23:38:52 -0400

    data digitada + dias

--- data/Program.cs
+++ data/Program.cs
@@ -13,14 +13,25 @@ namespace data
         [STAThreadAttribute]
         static void Main(string[] args)
         {
-            DateTime dataAtual = DateTime.Now;
+            DateTime dataAtual;
+
+            if (args.Length > 1)
+            {
+                dataAtual = DateTime.Parse(args[1]);
+            }
+            else
+            {
+                dataAtual = DateTime.Now;
+            }
+            
             DateTime dataResultado = dataAtual.AddDays(int.Parse(args[0]));
             String dataString = dataResultado.ToString("dd/MM/yyyy");
             //Console.WriteLine("Numero digitado foi: " + args[0]);
-            Console.WriteLine("Data atual: " + dataAtual.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Data inicial: " + dataAtual.ToString("dd/MM/yyyy")+" somando "+args[0]+" dias");
             Clipboard.SetText(dataString);
             Console.WriteLine("Data resultado: " + dataString);
             Console.ReadLine();
+            
         }
     }
 }
